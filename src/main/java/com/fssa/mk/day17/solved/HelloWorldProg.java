32ee9da2cc4c6b7662782d1f2edc89682ad51f82
Mathankumar G N <116251925+mathangn@users.noreply.github.com>
2023-06-23 10:18:44 +0530

    Add files via upload

---
+++ src/main/java/com/fssa/mk/day17/solved/HelloWorldProg.java
@@ -0,0 +1,11 @@
+package com.fssa.mk.day17.solved;
+
+public class HelloWorldProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World..!");
+
+	}
+
+}
